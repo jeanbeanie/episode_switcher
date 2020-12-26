ce87cd0007c107ad9a3bba3bb3c93e3adb1bedd5
jeanbeanie <msjeanneramos@gmail.com>
2020-12-26 11:26:16 -0500

    added logic to fetch basic show details, added axios, removed some boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
+interface IShowState {
+  id: number,
+  name: string,
+  summary: string,
+  premiereDate: string,
+  imageURL: string,
+}
 
 function App() {
+  const defaultShow : IShowState = {id:0, name:"", summary:"", premiereDate:"", imageURL:""}
+  const [show, setShow] = useState(defaultShow);
+ 
+  useEffect(() => {
+    async function fetchData () {
+      const result = await axios('http://api.tvmaze.com/search/shows?q=girls');
+      console.log(result)
+      const fetchedShow = result.data[0].show;
+      const {id, name, summary, premiered, image} = fetchedShow;
+      const showState = {
+        id,
+        name,
+        summary,
+        premiereDate: premiered,
+        imageURL: image.medium,
+      }
+      setShow(showState);
+    }
+
+    fetchData();
+  },[]);
+ 
+
+  console.log('SHOW STATE', show)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Show: {show.name}
       </header>
     </div>
   );
