1e8d7e23b357eca8f033cb307cefd94071806939
jeanbeanie <msjeanneramos@gmail.com>
2020-12-27 13:34:50 -0500

    add placeholder image

--- src/ImageTextCard.tsx
+++ src/ImageTextCard.tsx
@@ -15,13 +15,19 @@ interface IProps {
 const StyledRow = styled(Row)`margin-top:1rem`;
 const SubTitle = styled.p`opacity:.5;`;
 const StyledImage = styled.img`max-width:100%;`;
+const ImagePlaceholder = styled.div`background-color:grey; height:100%;color:white;text-align:center;font-size:3rem;`;
+
 
 const ImageTextCard = (props: IProps) => {
   const {smallTitle, imageURL, imageAlt, title, subTitle, body} = props;
   return(
     <StyledRow>
       <Col xs="3">
-        <StyledImage src={imageURL} alt={imageAlt}/>
+        {
+          imageURL ? 
+          <StyledImage src={imageURL} alt={imageAlt}/>
+            : <ImagePlaceholder>NA</ImagePlaceholder>
+        }
       </Col>
       <Col xs="9">
         {
